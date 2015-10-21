34e9b6ea3d43ea14e4088f39c632a86ac8d24935
Sachin <sravich2@illinois.edu>
2015-10-21 12:20:37 +0530

    Search functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
-
+  def self.search(string)
+    conditions = string.split(',').collect do |m|
+      m.strip!
+      delim = (m.include?('=') ? '=' : 'like')
+      expr = m.split(/=|like/i)
+      "#{expr[0].strip.downcase.tr(' ', '_')} #{delim} '#{expr[1].strip}'"
+         end
+    final_condition = conditions.join(' AND ')
+    puts final_condition
+    Product.where(final_condition)
+  end
 end
+
