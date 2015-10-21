7c1fff1175ba27ee2322d2f4cd84c3d34b67ece8
Sachin <sravich2@illinois.edu>
2015-10-21 10:39:20 +0530

    Stylistic changes. Single table inheritance for products.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021010943) do
+ActiveRecord::Schema.define(version: 20151021045955) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151021010943) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "img_url"
+    t.string   "type"
   end
 
   create_table "users", force: :cascade do |t|
