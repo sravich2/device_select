584983abfa129758d26c6d2cac97580687d5718b
Sachin <sravich2@illinois.edu>
2015-12-04 12:01:25 -0600

    a

--- app/jobs/fetch_critic_reviews_job.rb
+++ app/jobs/fetch_critic_reviews_job.rb
@@ -10,10 +10,11 @@ class FetchCriticReviewsJob < ActiveJob::Base
               'div.border-bottom:nth-child(5) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)',
               'div.border-bottom:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)']
     xpaths.each do |xp|
+      begin
+
       q = page.search(xp).text.delete("\n").strip
       page2 = agent.get("http://www.google.com/search?q=#{q}")
       url = page2.links_with(:href => %r"/url.+").first.href.split('/url?q=')[1].split('&sa')[0]
-      begin
         html, title, author, published = Product.generate_readable_html(url)
         summary = Product.generate_summary(html)
         CriticReview.create(:product_id => product.id, :url => url, :page_html => html, :title => title, :author => author, :published => published, :summary => summary)
