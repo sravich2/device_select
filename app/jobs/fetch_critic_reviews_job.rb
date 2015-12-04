6d4cba9875cc8a52e06181495e4dadbf58a5b1f6
Sachin <sravich2@illinois.edu>
2015-12-04 10:42:18 -0600

    a

--- app/jobs/fetch_critic_reviews_job.rb
+++ app/jobs/fetch_critic_reviews_job.rb
@@ -12,10 +12,13 @@ class FetchCriticReviewsJob < ActiveJob::Base
       xpaths.each do |xp|
         q = page.search(xp).text.delete("\n").strip
         page2 = agent.get("http://www.google.com/search?q=#{q}")
-        url = page2.links[24].href.split('/url?q=')[1].split('&sa')[0]
+        url = page2.links_with(:href=>%r"/url.+").first.href.split('/url?q=')[1].split('&sa')[0]
+        begin
         html, title, author, published = Product.generate_readable_html(url)
         summary = Product.generate_summary(html)
         CriticReview.create(:product_id => product.id, :url => url, :page_html => html, :title => title, :author => author, :published => published, :summary => summary)
+        rescue
+          end
       end
   end
 end
